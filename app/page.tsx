8b539379c373c747247096489b1d8920af224f93
manlikexavy <chibokaxavier@gmail.com>
2025-01-23 19:45:34 +0100

    Installeed Redux Toolkit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-900">
-fghjkn
-    </div>
-  );
+  return <div className="bg-blue-900"></div>;
 }
