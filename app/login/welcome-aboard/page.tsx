8162921e544f1af510cbd5699601f5cf04d64445
manlikexavy <chibokaxavier@gmail.com>
2025-01-26 09:56:19 +0100

    welcome abord page complete

--- app/login/welcome-aboard/page.tsx
+++ app/login/welcome-aboard/page.tsx
@@ -6,7 +6,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 
-
 const page = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const handleSignIn = (e: any) => {
@@ -49,9 +48,7 @@ const page = () => {
           priority
         />
         <div className="absolute bottom-20 left-0 w-full flex items-center justify-center flex-col mb-5 z-20">
-          <p className="text-3xl font-medium text-white">
-            Pay less to send more.
-          </p>
+          <p className="text-3xl font-medium text-white">Setup complete.</p>
           <p className="text-sm w-[323px] mt-10 font-medium text-center text-white">
             Your money deserves better. Send money internationally with the
             lowest transaction fees.
@@ -59,61 +56,25 @@ const page = () => {
         </div>
       </div>
 
-      <div className="bg-white flex-1 flex items-center lg:items-start px-8  lg:px-0  lg:pt-28 justify-center min-h-screen relative">
+      <div className="bg-white flex-1 flex items-center  px-8  lg:px-0  lg:pt-28 justify-center min-h-screen relative">
         <div className="absolute inset-0 bg-white  opacity-50 z-10"></div>
         <div className="absolute inset-0 bg-white  opacity-50 z-10"></div>
         <div className="absolute inset-0 bg-white  opacity-50 z-10"></div>
         <div className="flex flex-col z-20 ">
           <p className="text-[#0A3C43] font-medium text-2xl  lg:px-0 leading-9">
-            Hello,
+            Welcome aboard,
           </p>
-          <p className="text-sm font-medium mb-5">Sign in to get started</p>
-          <form className="space-y-4 mt-4 ">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email address</Label>
-              <Input
-                id="email"
-                type="email"
-                name="email"
-                value={formData.email}
-                className="w-96 h-14 rounded-lg text-lg outline-none border-[1px] border-black focus:border-none focus:outline-none"
-                onChange={handleSignIn}
-                placeholder="johndoe@gmail.com"
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="current">Password</Label>
-              <Input
-                id="current"
-                type="password"
-                name="password"
-                className="w-96 h-14 rounded-lg  outline-none border-[1px] border-black focus:border-none focus:outline-none"
-                value={formData.password}
-                onChange={handleSignIn}
-              />
-            </div>
-            <div className="lg:pt-10 pt-8">
-              <p className="font-medium text-sm ">
-                Forgot Password ?{" "}
-                <span className="text-green-600  mb-5">Reset Here</span>
-              </p>
-              <Button
-                className="w-96 h-14 mt-5 rounded-md bg-[#0A3C43] text-white"
-                type="submit"
-              >
-                Login
-              </Button>
-            </div>
-          </form>
+          <p className="text-sm font-medium mt-2">
+            Let’s get you signed in with your new credentials
+          </p>
+          <Button
+            className="w-96 h-14 mt-52 lg:mt-20 rounded-md bg-[#0A3C43] text-white"
+            type="submit"
+          >
+            Get started
+          </Button>
           <div className="absolute bottom-5">
-            <p className="font-medium text-sm mb-5 ">
-              New here?{" "}
-              <Link href={'/signup'}>
-                {" "}
-                <span className="text-green-600">Create account</span>
-              </Link>
-            </p>{" "}
-            <p className="font-medium text-sm">
+            <p className="font-medium text-sm pr-4">
               {" "}
               © 2024 Kria Technologies Ltd. All rights reserved.{" "}
               <span className="text-green-600 underline">
