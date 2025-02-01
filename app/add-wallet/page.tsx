6cda0163e95951528064b6923ab56b5bff91b345
manlikexavy <chibokaxavier@gmail.com>
2025-02-01 18:41:44 +0100

    add wallet page complete

--- app/add-wallet/page.tsx
+++ app/add-wallet/page.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { Avatar, Select, SelectedItems, SelectItem } from "@heroui/react";
+import { ArrowUpDownIcon } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+type Country = {
+  key: string;
+  label: string;
+  flag: string;
+};
+
+export const countries = [
+  { key: "nigeria", label: "Nigeria", flag: "/nigeriaflag.svg" },
+  { key: "sierra-leone", label: "Sierra Leone", flag: "/sierraflag.svg" },
+];
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center tracking-[-0.5] lg:h-[50vh]">
+      <div className="text-left ">
+        <p className="font-medium text-lg mb-3 ">Add Wallet</p>
+        <p className="text-sm font-normal  mb-5">
+          Add from our selected wallets{" "}
+        </p>
 
-export default page
+        <Select
+          className="w-[380px] text-sm text-black"
+          label="Select wallet type"
+          size="lg"
+          items={countries}
+          renderValue={(items: SelectedItems<Country>) => {
+            return items.map((item) => (
+              <div key={item.key} className="flex items-center gap-2 py-2">
+                <Image
+                  src={item.data!.flag}
+                  alt={item.data!.label}
+                  height={27}
+                  width={27}
+                  className="rounded-full my-10"
+                  priority
+                />
+
+                <div className="flex flex-col">
+                  <span>{item.data?.label}</span>
+                </div>
+              </div>
+            ));
+          }}
+        >
+          {(country) => (
+            <SelectItem>
+              <div className="flex gap-3 items-center">
+                <Image
+                  src={country.flag}
+                  alt="ngn logo"
+                  height={27}
+                  width={27}
+                  priority
+                  className="rounded-full"
+                />{" "}
+                {country.label}
+              </div>
+            </SelectItem>
+          )}
+        </Select>
+      </div>
+      <Button
+        className="w-96 h-[45px] mt-32 rounded-md bg-[#0A3C43] text-white disabled:bg-gray-300"
+        type="submit"
+      >
+        {"Select"}
+      </Button>
+    </div>
+  );
+};
+
+export default page;
